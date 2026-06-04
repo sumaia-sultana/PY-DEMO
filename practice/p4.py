4588b48bcaa945f4bb249f2b63196ec215b27a6c
Sumaia Sultana <134152349+sumaia-sultana@users.noreply.github.com>
2026-06-04 09:49:45 +0600

    solve 3 more exercise

--- practice/p4.py
+++ practice/p4.py
@@ -1,1 +1,7 @@
-# names = ["Sumaiya", "Rahim", "Karim"]
+# names = ["Sumaiya", "Rahim", "Karim"] 
+# Use a loop to print each name.
+
+names = ["Sumaiya", "Rahim", "Karim"]
+
+for x in names:
+  print(x)
---
+++ practice/p5.py
@@ -0,0 +1,10 @@
+#  foods = ["rice", "fish", "meat", "egg"] 
+# Print the total number of items.
+
+foods = ["rice", "fish", "meat", "egg"] 
+
+x = len(foods)
+
+print(x)
+
+ 
